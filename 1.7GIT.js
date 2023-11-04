d30ef8de35f7ce0181298b2f59aff0e8f34e0649
Cristina Oprisan <oprisan.cristina29@gmail.com>
2023-11-04 20:59:11 +0100

    First commit

---
+++ 1.7GIT.js
@@ -0,0 +1,30 @@
+function sum(op1, op2) {
+    return op1 + op2;
+}
+
+console.log(sum(2, 7));
+
+function sub(op1, op2) {
+    return op1 - op2;
+}
+
+console.log(sub(2, 7));
+
+function div(op1, op2) {
+    return op1 / op2;
+}
+
+console.log(div(2, 7));
+
+function mult(op1, op2) {
+    return op1 * op2;
+}
+
+console.log(mult(2, 7));
+
+function cuadrado(numero) {
+    return numero * numero
+}
+
+let number = 3
+console.log(cuadrado(number));
